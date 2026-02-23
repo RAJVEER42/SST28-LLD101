c786cc713338d294bb208c44af3418ef26ac1cd7
RAJVEER42 <rajveer.24bcs10404@sst.scaler.com>
2026-02-23 09:47:00 +0530

    SOLID Exercise 6 : SOLID Principles Combined

---
+++ SOLID/Ex6/src/EmailSender.java
@@ -0,0 +1,11 @@
+public class EmailSender extends NotificationSender {
+    public EmailSender(AuditLog audit) { super(audit); }
+
+    @Override
+    public void send(Notification n) {
+        // LSP smell: truncates silently, changing meaning
+        String body = n.body;
+        System.out.println("EMAIL -> to=" + n.email + " subject=" + n.subject + " body=" + body);
+        audit.add("email sent");
+    }
+}
